1cb66863a1afc977e5690416cd01e72067f248be
JonathanDicky <jonthzee@gmail.com>
2026-06-20 00:42:10 +0700

    database siap pakai tanpa migrate

--- backend/config/database.go
+++ backend/config/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,19 +13,35 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+	var dsn string
+	rawURL := os.Getenv("DATABASE_URL")
+
+	if rawURL != "" {
+
+		dsn = strings.TrimPrefix(rawURL, "mysql://")
+		if strings.Contains(dsn, "?") {
+			dsn += "&parseTime=True&loc=Local"
+		} else {
+			dsn += "?parseTime=True&loc=Local"
+		}
+		log.Println("--- KONEK KE DB PRODUCTION (RAILWAY) ---")
+	} else {
+
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_NAME"),
+		)
+		log.Println("--- KONEK KE DB LOKAL (LOCALHOST) ---")
+	}
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		log.Fatal("Gagal konek database: ", err)
 	}
 
 	DB = db
-	log.Println("Database connected successfully")
+	log.Println("Database sukses terhubung!")
 }
